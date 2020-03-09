77584ba8451bbc46c57e9e91fcc038345c8de53d
amin ashtiani <amin.ashtiani@snapp.market>
2020-03-09 21:45:28 +0330

    handle some criteria
    remove duplicated code and convert them into one function
    check condition with if

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,61 +1,77 @@
- const defaultResult = 0;
- let currentResult = 0;
- const logEntries = [];
+const defaultResult = 0;
+let currentResult = 0;
+const logEntries = [];
 
- function getUserNumberInput() {
-     return parseInt(userInput.value);
- }
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
 
- function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-     outputResult(currentResult, calcDescription)
- }
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
 
- function createAndPushLog(operator, number, prevResult) {
-     const logEntry = {
-         number,
-         operator,
-         result: currentResult,
-         prevResult,
-     }
-     logEntries.push(logEntry);
- }
+function calculateResult(calculationType) { // calculationType ADD SUBTRACT MULTIPLY DIVIDE
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    let mathOperator;
+    if (calculationType !== 'ADD' &&
+        calculationType !== 'SUBTRACT' &&
+        calculationType !== 'MULTIPLY' &&
+        calculationType !== 'DIVIDE' ||
+        !enteredNumber) {
+        return;
+    }
+    if (calculationType === 'ADD') {
+        currentResult += enteredNumber;
+        mathOperator = "+";
+    } else if (calculationType === 'SUBTRACT') {
+        currentResult -= enteredNumber;
+        mathOperator = "-";
 
- function add() {
-     const enteredNumber = getUserNumberInput();
-     const initialResult = currentResult;
-     currentResult = currentResult + enteredNumber;
-     createAndPushLog('+', enteredNumber, initialResult);
-     createAndWriteOutput('+', initialResult, enteredNumber);
- }
+    } else if (calculationTypr === 'DIVIDE') {
+        currentResult /= enteredNumber;
+        mathOperator = "/";
 
-
- function subtract() {
-     const enteredNumber = getUserNumberInput();
-     const initialResult = currentResult;
-     currentResult = currentResult - enteredNumber;
-     createAndPushLog('-', enteredNumber, initialResult);
-     createAndWriteOutput('-', initialResult, enteredNumber);
- }
-
- function divide() {
-     const enteredNumber = getUserNumberInput();
-     const initialResult = currentResult;
-     currentResult = currentResult / enteredNumber;
-     createAndPushLog('/', enteredNumber, initialResult);
-     createAndWriteOutput('/', initialResult, enteredNumber);
- }
-
- function multiply() {
-     const enteredNumber = getUserNumberInput();
-     const initialResult = currentResult;
-     currentResult = currentResult * enteredNumber;
-     createAndPushLog('*', enteredNumber, initialResult);
-     createAndWriteOutput('*', initialResult, enteredNumber);
- }
+    } else if (calculationType === 'MULTIPLY') {
+        currentResult *= enteredNumber;
+        mathOperator = "*";
+    }
+    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+    createAndPushLog(mathOperator, enteredNumber, initialResult)
+}
 
 
- addBtn.addEventListener('click', add);
- multiplyBtn.addEventListener('click', multiply);
- divideBtn.addEventListener('click', divide);
- subtractBtn.addEventListener('click', subtract);
+
+
+function createAndPushLog(operator, number, prevResult) {
+    const logEntry = {
+        number,
+        operator,
+        result: currentResult,
+        prevResult
+    };
+    logEntries.push(logEntry);
+}
+
+function add() {
+    calculateResult('ADD');
+}
+
+function subtract() {
+    calculateResult('SUBTRACT');
+}
+
+function divide() {
+    calculateResult('DIVIDE');
+}
+
+function multiply() {
+    calculateResult('MULTIPLY');
+}
+
+addBtn.addEventListener("click", add);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+subtractBtn.addEventListener("click", subtract);
+subtractBtn.addEventListener("click", subtract);
